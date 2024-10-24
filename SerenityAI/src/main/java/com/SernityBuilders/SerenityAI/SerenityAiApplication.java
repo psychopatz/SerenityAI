ca30f4c70c3d62c4952c9458a0a2b3eb88ab5e1f
Psychopatz <bpatrickoliverb@gmail.com>
2024-10-24 15:24:50 +0800

    first commit

---
+++ SerenityAI/src/main/java/com/SernityBuilders/SerenityAI/SerenityAiApplication.java
@@ -0,0 +1,13 @@
+package com.SernityBuilders.SerenityAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerenityAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerenityAiApplication.class, args);
+	}
+
+}
---
+++ SerenityAI/src/test/java/com/SernityBuilders/SerenityAI/SerenityAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SernityBuilders.SerenityAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerenityAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

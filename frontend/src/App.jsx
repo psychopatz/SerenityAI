975d2baa3e40f30726b2efed141671e5e22330b9
Psychopatz <bpatrickoliverb@gmail.com>
2024-11-14 15:06:23 +0800

    Implement Nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './component/Register';
 import Login from './component/LoginRegister';
@@ -6,22 +6,22 @@ import MoodEntryApp from './component/MoodEntryApp';
 import ChatInterface from './component/ChatInterface';
 import UserList from './component/UserList';
 import RecommendationApp from './component/RecommendationApp';
-
+import Nav from './component/Nav';
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/moodentry" element={<MoodEntryApp />} />
-                <Route path="/chat" element={<ChatInterface/>} />
-                <Route path="/list" element={<UserList/>} />
-                <Route path="/recommendation" element={<RecommendationApp/>} />
-
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/moodentry" element={<MoodEntryApp />} />
+        <Route path="/chat" element={<ChatInterface/>} />
+        <Route path="/list" element={<UserList/>} />
+        <Route path="/recommendation" element={<RecommendationApp/>} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;

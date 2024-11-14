2d2058b7b28e51ccdb10d10a3ad4a8cbf4001880
ShaneAdrianOpinion <shaneopinion95@gmail.com>
2024-11-15 01:16:45 +0800

    Nav Adjusted

--- frontend/src/component/Nav.jsx
+++ frontend/src/component/Nav.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import React from "react";
 import { useLocation } from 'react-router-dom';
 import { navBackgroundImage } from "../styles/NavStyles";
+
 
 const Nav = () => {
   const location = useLocation();
@@ -31,15 +32,15 @@ const Nav = () => {
   console.log("Rendering Navigation component");
 
   return (
-    <AppBarStyled position="static" style={{ marginTop: '1rem', paddingTop: '1rem' }}>
+    <AppBarStyled position="static" sx={{ width: '1520px' }} style={{ marginTop: '1rem', paddingTop: '1rem'}}>
       <Toolbar>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
+        <Typography variant="h6" style={{ flexGrow: 1, marginTop: '10px', color:'black', fontWeight: 'bold' }}>
           Serenity AI
         </Typography>
         {navItems.map((item, index) => {
           console.log(`Rendering nav item: ${item.label}`);
           return (
-            <Button key={index} style={navLinkStyle} href={item.path}>
+            <Button key={index} style={{ ...navLinkStyle, marginTop: '15px'}} href={item.path}>
               {item.label}
             </Button>
           );
